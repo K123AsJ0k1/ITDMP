1479480d8737751c9fd7caae36cfe354e29c0f3d
Niila Siilasjoki <niila.siilasjoki@gmail.com>
2022-09-22 15:51:30 +0300

    Front and back connected

--- backend/app.py
+++ backend/app.py
@@ -1,7 +1,16 @@
-from flask import Flask
+from flask import Flask, jsonify, request
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route("/")
 def index():
-    return "Heipparallaa!"
+    return "Index"
+
+@app.route("/api/model", methods=["get","post"])
+def model():
+    data = request.json
+    #text = request.json['text']
+    print(data)
+    return jsonify({'status': 200}) 
