3926830a268b4c7513d016a563728d1cf0ac0683
Niila Siilasjoki <niila.siilasjoki@gmail.com>
2022-10-06 15:01:30 +0300

    Heroku deployment test

--- backend/app.py
+++ backend/app.py
@@ -1,16 +1,21 @@
-from flask import Flask, jsonify, request
-from flask_cors import CORS
+from flask import Flask, jsonify, request, send_from_directory
+from flask_cors import CORS, cross_origin
 
-app = Flask(__name__)
+app = Flask(__name__,static_folder='../frontend/build',static_url_path='')
+#app = Flask(__name__)
 CORS(app)
 
-@app.route("/")
-def index():
-    return "Index"
-
+@cross_origin()
 @app.route("/api/model", methods=["get","post"])
 def model():
     data = request.json
     #text = request.json['text']
     print(data)
     return jsonify({'status': 200}) 
+
+@app.route("/")
+def serve():
+    return send_from_directory(app.static_folder, 'index.html')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
